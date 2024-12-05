c1a7d25dfbc9de06624c2ab0e3964d3eb468ad34
ali noori <alinoori021110@gmail.com>
2024-12-05 15:11:53 +0330

    feat: implement the auth module

---
+++ src/modules/auth/auth.controller.js
@@ -0,0 +1,36 @@
+const { AuthMessage } = require("./auth.messages");
+const authService = require("./auth.service");
+const autoBind = require("auto-bind");
+
+class AuthController {
+  #service;
+  constructor() {
+    autoBind(this);
+    this.#service = authService;
+  }
+  async sendOTP(req, res, next) {
+    try {
+      const { mobile } = req.body;
+       await this.#service.sendOTP(mobile);
+      return {
+        message:AuthMessage.SendOtpSuccessfully
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+  async checkOTP(req, res, next) {
+    try {
+    } catch (error) {
+      next(error);
+    }
+  }
+  async logout(req, res, next) {
+    try {
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+module.exports = new AuthController();
---
+++ src/modules/auth/auth.messages.js
@@ -0,0 +1,13 @@
+const AuthMessage = {
+    SendOtpSuccessfully:"sent otp successfully",
+    NotFound:"user not found",
+    OtpCodeNotExpired :"otp code not expired please try again later"
+
+
+
+}
+
+module.exports = {
+    AuthMessage
+
+}
